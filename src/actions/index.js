31f674dab5a9079963c4084822af0ed406f464c1
musab.akram <musab.akram@numumail.com>
2019-01-10 09:37:32 +0500

    memoize function added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,11 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
+
+// export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+//   await dispatch(fetchPosts());
+//   console.log(getState().posts);
+// };
+
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
   dispatch({
@@ -7,10 +14,13 @@ export const fetchPosts = () => async dispatch => {
   });
 };
 
-export const fetchUser = id => async dispatch => {
+export const fetchUser = id => dispatch => {
+  _fetch(id, dispatch);
+};
+const _fetch = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({
     type: "FETCH_USER",
     payload: response.data
   });
-};
+});
